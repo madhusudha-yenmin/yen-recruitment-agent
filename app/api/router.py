9e64543a6c1f76814b1448852ee0909340a32eb3
Sidhu-yenmin <sidhu.yenmin@gmail.com>
2026-07-10 13:53:07 +0530

    Implemented parsing of resume

--- app/api/router.py
+++ app/api/router.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter
 
 api_router = APIRouter()
 
-from app.api.v1 import auth, recruitment, interview
+from app.api.v1 import auth, recruitment, interview, resume
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(recruitment.router, prefix="/recruitment", tags=["recruitment"])
 api_router.include_router(interview.router, prefix="/interview", tags=["interview"])
+api_router.include_router(resume.router, prefix="/resume", tags=["resume"])
 
 # Future routers:
 # from app.api.v1 import jobs, candidates
